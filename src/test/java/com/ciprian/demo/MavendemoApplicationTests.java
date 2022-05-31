497fb5d7dfca084415ce84212fc35691a3059732
csciprian94 <csicsek.ciprian@innobyte.hu>
2022-05-31 16:17:15 +0200

    projekt init

---
+++ src/test/java/com/ciprian/demo/MavendemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciprian.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavendemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
