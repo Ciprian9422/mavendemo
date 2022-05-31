d400a02a259a089b1bfc5f6848b0efa67edac7ea
csciprian94 <csicsek.ciprian@innobyte.hu>
2022-05-31 17:03:22 +0200

    - thymeleaf demo

---
+++ src/main/java/com/ciprian/demo/controller/PersonController.java
@@ -0,0 +1,23 @@
+package com.ciprian.demo.controller;
+
+import com.ciprian.demo.domain.Person;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+
+@Controller
+public class PersonController {
+
+  @GetMapping
+  String getPeople(Model model) {
+    Person person1 = new Person();
+    person1.setAge(18).setName("Ágnes");
+
+    model.addAttribute("something", "List of people");
+    model.addAttribute("people",
+        Arrays.asList(new Person("Károly", 28), new Person("Krisztián", 30), person1));
+    return "people";
+  }
+}
---
+++ src/main/java/com/ciprian/demo/domain/Person.java
@@ -0,0 +1,18 @@
+package com.ciprian.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class Person {
+
+  private String name;
+  private Integer age;
+}
